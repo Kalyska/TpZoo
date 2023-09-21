7d4cbabc297e9ec888c8010e85e06b98c2bcff85
Rémy Dumont <remy.dumont@labom2iformation.fr>
2023-09-21 12:03:31 +0200

    Add : Datas (ZooCore) -> Seed (Animals) v1

---
+++ ZooCore/Datas/InitialAnimal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooCore.Datas
+{
+    public static class InitialAnimal
+    {
+
+        public static readonly List<Animal> Animals = new List<Animal>()
+        {
+            new Animal() { Id = 1, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/Alligator.webp" },
+
+            new Animal() { Id = 2, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/Calao" },
+
+            new Animal() { Id = 3, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 4, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 5, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 6, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 7, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 8, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 9, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 10, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 11, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 12, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 13, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+            new Animal() { Id = 14, Name="", Age = 0, Description ="", Status = 0,ImageUrl = "/images/" },
+
+        };
+
+    }
+}
