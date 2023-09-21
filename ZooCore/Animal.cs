f4fee6438c46d4429c98eee9e490c38a7197f848
Clemence <penin.clemence@gmail.com>
2023-09-21 11:37:58 +0200

    Ajout models Species + Animal (Core)

---
+++ ZooCore/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooCore
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Species? Species { get; set; }
+        public string? Description { get; set; }
+        public int Age { get; set; }
+        public string? ImageUrl { get; set; }
+        public ConservationStatus Status { get; set; }
+    }
+
+
+    public enum ConservationStatus
+    {
+        Unknown,
+        LeastConcern,
+        NearThreatened,
+        Vulnerable,
+        Endangered,
+        CriticallyEndangered,
+        ExtinctInTheWild,
+        Extinct
+    }
+}
---
+++ ZooCore/Species.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooCore
+{
+    public class Species
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public IEnumerable<Animal>? Animals { get; set; }
+    }
+}
