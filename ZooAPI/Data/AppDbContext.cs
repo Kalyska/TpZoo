616474ed0bff430c9943b7517c29220fcd60ccd4
Clemence <penin.clemence@gmail.com>
2023-09-21 14:13:07 +0200

    Add AppDbContext

---
+++ ZooAPI/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZooCore.Datas;
+using ZooCore.Models;
+
+namespace ZooAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Species> Species { get; set; }
+        public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Species>().HasData(InitialSpecies.zooSpecies);
+            modelBuilder.Entity<Animal>().HasData(InitialAnimal.Animals);
+        }
+    }
+}
