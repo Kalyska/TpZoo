c735012664f9925c3911bf81f37f7ef9634274e3
Clemence <penin.clemence@gmail.com>
2023-09-21 11:53:59 +0200

    ajout DataAnnotations sur model Species + transfert de la propriété ConservationStatus

---
+++ ZooCore/Models/Species.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooCore.Models
+{
+    public class Species
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public ConservationStatus Status { get; set; }
+        public List<Animal>? Animals { get; set; }
+    }
+
+    public enum ConservationStatus
+    {
+        Unknown,
+        LeastConcern,
+        NearThreatened,
+        Vulnerable,
+        Endangered,
+        CriticallyEndangered,
+        ExtinctInTheWild,
+        Extinct
+    }
+}
