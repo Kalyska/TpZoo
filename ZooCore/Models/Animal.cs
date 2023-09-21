c5adbfdb4861a22e3b77c46806d311d04ce283ae
Clemence <penin.clemence@gmail.com>
2023-09-21 11:53:11 +0200

    ajout des DataAnnotations sur models Animal

--- ZooCore/Models/Animal.cs
+++ ZooCore/Models/Animal.cs
@@ -1,32 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZooCore
+namespace ZooCore.Models
 {
     public class Animal
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+
+        [ForeignKey(nameof(Species))]
+        public int SpeciesId { get; set; }
         public Species? Species { get; set; }
         public string? Description { get; set; }
         public int Age { get; set; }
         public string? ImageUrl { get; set; }
-        public ConservationStatus Status { get; set; }
-    }
-
-
-    public enum ConservationStatus
-    {
-        Unknown,
-        LeastConcern,
-        NearThreatened,
-        Vulnerable,
-        Endangered,
-        CriticallyEndangered,
-        ExtinctInTheWild,
-        Extinct
     }
 }
