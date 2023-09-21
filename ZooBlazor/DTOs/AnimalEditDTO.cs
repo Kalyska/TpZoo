61433666e6d2360124462eeb4f9cca9d547e3a25
Rémy Dumont <remy.dumont@labom2iformation.fr>
2023-09-21 14:50:13 +0200

    Feature : Folder : DTOs -> AnimalEditDTO

---
+++ ZooBlazor/DTOs/AnimalEditDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ZooCore.Models;
+
+namespace ZooBlazor.DTOs
+{
+    public class AnimalEditDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Le Nom est requis.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "L'espèce est requise.")]
+        public int SpeciesId { get; set; }
+
+        [Required(ErrorMessage = "La description est requise.")]
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(1, 99, ErrorMessage = "L'age doit être compris entre 1 et 99 €.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Le lien vers l'image est requis.")]
+        public string? ImageUrl { get; set; }
+    }
+}
